a845a895da6cceb1c249410074b8d6fbd4f7cba1
ludndev <ludndev@gmail.com>
2024-07-15 15:38:02 +0100

    feat: add tests for password generation in test.rs

--- src/test.rs
+++ src/test.rs
@@ -61,3 +61,67 @@ fn parse_args_invalid_complexity() {
 
     assert!(result.is_err()); // Expect an error
 }
+
+#[test]
+fn generate_password_length() {
+    let length = 16;
+    let password = generate_password(length, false, &ComplexityEnum::Simple);
+
+    assert_eq!(password.len(), length);
+}
+
+#[test]
+fn generate_password_special_chars() {
+    let length = 16;
+    let password = generate_password(length, true, &ComplexityEnum::Complex);
+
+    assert!(password.chars().any(|c| "!@#$%^&*()_+-=[]{}|;:,.<>?".contains(c)));
+}
+
+#[test]
+fn generate_password_no_special_chars() {
+    let length = 16;
+    let password = generate_password(length, false, &ComplexityEnum::Complex);
+
+    assert!(!password.chars().any(|c| "!@#$%^&*()_+-=[]{}|;:,.<>?".contains(c))); /// @todo: fail sometimes
+}
+
+#[test]
+fn generate_password_complexity_simple() {
+    let length = 16;
+    let password = generate_password(length, false, &ComplexityEnum::Simple);
+
+    assert!(password.chars().all(|c| "abcdefghijklmnopqrstuvwxyz".contains(c)));
+}
+
+#[test]
+fn generate_password_complexity_secure() {
+    let length = 16;
+    let password = generate_password(length, false, &ComplexityEnum::Secure);
+
+    assert!(password.chars().any(|c| "ABCDEFGHIJKLMNOPQRSTUVWXYZ".contains(c)));
+    assert!(password.chars().any(|c| "0123456789".contains(c)));
+}
+
+#[test]
+fn generate_password_complexity_complex() {
+    let length = 16;
+    let password = generate_password(length, true, &ComplexityEnum::Complex);
+
+    assert!(password.chars().any(|c| "abcdefghijklmnopqrstuvwxyz".contains(c)));
+    assert!(password.chars().any(|c| "ABCDEFGHIJKLMNOPQRSTUVWXYZ".contains(c))); /// @todo: fail sometimes
+    assert!(password.chars().any(|c| "0123456789".contains(c)));
+    assert!(password.chars().any(|c| "!@#$%^&*()_+-=[]{}|;:,.<>?".contains(c)));
+}
+
+#[test]
+fn generate_multiple_passwords() {
+    let length = 16;
+    let quantity = 5;
+    let passwords: Vec<String> = (0..quantity)
+        .map(|_| generate_password(length, true, &ComplexityEnum::Secure))
+        .collect();
+
+    let unique_passwords: std::collections::HashSet<_> = passwords.iter().collect();
+    assert_eq!(unique_passwords.len(), quantity); // Ensure all generated passwords are unique
+}
